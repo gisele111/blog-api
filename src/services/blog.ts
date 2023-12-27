8a0221d98e88b09928c4535611acee6a3d70312c
zachee1 <zachee.ishimwe@amalitech.org>
2023-12-27 17:04:14 +0200

    get single item

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -11,14 +11,20 @@ const createBlog = async (title:string,content:string)=>{
     return createdData
 }
 
-// const getAll 
-
 const getAll = async()=>{
     const allData = await prisma.blog.findMany();
     return allData;
 }
 
+const getSingleBlog = async(singleId:number)=>{
+const singleBlog = await prisma.blog.findUnique({
+    where:{id:singleId}
+});
+return singleBlog
+}
+
 export {
     createBlog,
-    getAll
+    getAll,
+    getSingleBlog
 };
